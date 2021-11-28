b995e3faf53d3b1d130a576eb86a57df4c44dc54
brayan obispo torres <segunda.bo@gmail.com>
2021-11-28 14:38:06 -0500

    Configuración para despliegue a producción

--- ecommerce_auth/wsgi.py
+++ ecommerce_auth/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecommerce_auth.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecommerce_auth.settings_prod')
 
 application = get_wsgi_application()
