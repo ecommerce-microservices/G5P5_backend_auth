95f677534a0d122343879f018a81de18ce678690
César Abril <cdgonzalezab@gmail.com>
2021-12-06 09:26:16 -0500

    complemento guía AuthMs

--- user/views.py
+++ user/views.py
@@ -1,12 +1,13 @@
 from django.conf import settings
 from rest_framework.response import Response
-from rest_framework import views, status
+from rest_framework import views, status, generics
 from user.serializers import UserSerializer
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.backends import TokenBackend
 from rest_framework_simplejwt.views import TokenVerifyView
 from rest_framework_simplejwt.exceptions import TokenError, InvalidToken
 from rest_framework_simplejwt.serializers import TokenVerifySerializer
+from user.models import User
 
 
 class UserAPIView(views.APIView):
@@ -26,6 +27,34 @@ class UserAPIView(views.APIView):
         except Exception as e:
             print(e)
             return Response('Error en la generación del token', status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+# User Detail View
+
+
+class UserDetailView(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    def get(self, request, *args, **kwargs):
+        return super().get(request, *args, **kwargs)
+
+
+class UserUpdateView(generics.UpdateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    def update(self, request, *args, **kwargs):
+        return super().update(request, *args, **kwargs)
+
+
+class UserDeleteView(generics.DestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    def delete(self, request, *args, **kwargs):
+        return super().destroy(request, *args, **kwargs)
+
+# Verify Token View
 
 
 class VerifyTokenView(TokenVerifyView):
